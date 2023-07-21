1c358a10ffd3c56040d0bfeb6d4a7674c401ed09
Beth Allaway <beth_allaway@yahoo.com>
2023-07-21 20:46:24 +0100

    fix some bits, add ability to edit and delete post, post detail stuff idk

--- profiles/views.py
+++ profiles/views.py
@@ -44,4 +44,3 @@ class ProfileDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
