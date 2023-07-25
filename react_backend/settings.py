da13d248e423089f4ca25862de04c10f4ceeb8c4
Beth Allaway <beth_allaway@yahoo.com>
2023-07-25 21:34:34 +0100

    added localhost origin

--- react_backend/settings.py
+++ react_backend/settings.py
@@ -106,7 +106,8 @@ MIDDLEWARE = [
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+        os.environ.get('CLIENT_ORIGIN'),
+        'http://localhost:3000',
     ]
 
 print('----- cors allowed origin')
