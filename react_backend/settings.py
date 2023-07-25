bbda57ca60359e28ed15a3cb478bbb00302241fa
Beth Allaway <beth_allaway@yahoo.com>
2023-07-25 21:31:38 +0100

    testing cors envs

--- react_backend/settings.py
+++ react_backend/settings.py
@@ -65,7 +65,6 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost',]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -109,10 +108,9 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
+
+print('----- cors allowed origin')
+print(CORS_ALLOWED_ORIGINS)
 
 CORS_ALLOW_CREDENTIALS = True
 
